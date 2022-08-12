7636aa133b4b8db4b4ddf85b3b5ae2187bd02496
expal <expal123@mail.ru>
2022-08-12 19:40:44 +0300

    hw3

---
+++ frontend/src/components/Menu.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Menu = () => {
+    return (
+
+        <ul>
+            <li><a href="#"><i className="fa fa-home fa-fw"></i>Главная страница</a></li>
+            <li><a href="#">Вход</a></li>
+            <li><a href="#">О компании</a></li>
+            <li><a href="#">Блог</a></li>
+            <li><a href="#">Контакты</a></li>
+        </ul>
+
+    )
+
+
+}
+
+export default Menu
---
+++ project_todo/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework.serializers import HyperlinkedModelSerializer
+from .models import Project
+
+
+class ProjectModelSerializer(HyperlinkedModelSerializer):
+    class Meta:
+        model = Project
+
+        fields = '__all__'
+
+
